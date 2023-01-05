29b199b3a365584023686818573ca8fed5136a28
Manhar-Dangar <73153726+Manhar-Dangar@users.noreply.github.com>
2023-01-05 11:32:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hello: "Agrisathi From Prod Server." }); 
+  res.send({ hello: "Welcome to AgriSathi." }); 
 });
 
 const PORT = process.env.PORT || 6000;
